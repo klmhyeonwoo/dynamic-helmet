9346b32975bdf2059ccd5562e94291adad13a22a
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-25 21:44:39 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,31 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import prerender from "@prerenderer/rollup-plugin";
 import path from "path";
+import chrome from 'chrome-aws-lambda'; // chrome-aws-lambda 가져오기
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(),prerender({
-    routes: ["/", "/test"],
-    renderer: "@prerenderer/renderer-puppeteer",
-    server: {
-      port: 3000,
-      host: "localhost",
-    },
-    rendererOptions: {
-      maxConcurrentRoutes: 1,
-      renderAfterTime: 500,
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'], // 실행 환경에 맞는 옵션
-    },
-    postProcess(renderedRoute) {
-      renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, "https:")
-          .replace(
-              /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
-              "https://dynamic-helmet.vercel.app/"
-          );
-    },
-  }),],
+  plugins: [
+    svgr(),
+    react(),
+    ...(process.env.VERCEL ? [
+      prerender({
+        routes: ["/", "/test"],
+        renderer: "@prerenderer/renderer-puppeteer",
+        server: {
+          port: 3000,
+          host: "localhost",
+        },
+        rendererOptions: {
+          headless: true,
+          args: chrome.args, // chrome-aws-lambda의 args 사용
+          executablePath: async () => {
+            return await chrome.executablePath; // chrome-aws-lambda의 executablePath 사용
+          },
+          ignoreHTTPSErrors: true,
+        },
+      })
+    ] : []), // 로컬에서는 프리렌더링 비활성화
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
