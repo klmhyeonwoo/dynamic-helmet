ba771682c75ba653052cde361f5e8d7f8a2cdf8f
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-25 22:05:57 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import prerender from "@prerenderer/rollup-plugin";
 import path from "path";
-import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    ...(process.env.VERCEL ? [
-      prerender({
-        routes: ["/", "/test"],
-        renderer: "@prerenderer/renderer-puppeteer",
-        server: {
-          port: 3000,
-          host: "localhost",
-        },
-        rendererOptions: {
-          launchOptions: {
-            headless: true,
-            args: chrome.args, // Vercel에서 필요한 args 설정
-            executablePath: chrome.executablePath || puppeteer.executablePath(), // 동기적으로 처리
-          },
-          ignoreHTTPSErrors: true,
-        },
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      })
-    ] : []), // 로컬에서는 프리렌더링 비활성화
-  ],
+  plugins: [svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
