f64e3287175d8d39864125947e03cedea9d118bd
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-25 21:24:43 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,20 @@ import svgr from "vite-plugin-svgr";
 import prerender from "@prerenderer/rollup-plugin";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(),prerender({
-    routes: ["/", "/test"],
-    renderer: "@prerenderer/renderer-puppeteer",
-    server: {
-      port: 3000,
-      host: "localhost",
-    },
-    rendererOptions: {
-      maxConcurrentRoutes: 1,
-      renderAfterTime: 500,
-    },
-    postProcess(renderedRoute) {
-      renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, "https:")
-          .replace(
-              /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
-              "https://dynamic-helmet.vercel.app/"
-          );
-    },
-  }),],
+  plugins: [
+    svgr(),
+    ...(process.env.PRE_RENDER === "true" ? [
+      prerender({
+        routes: ["/", "/test"],
+        renderer: "@prerenderer/renderer-puppeteer",
+        rendererOptions: {
+          headless: true,
+          renderAfterTime: 500,
+        },
+      }),
+    ] : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
