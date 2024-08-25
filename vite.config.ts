af9fa76c21703fef568b1ecd7fbdab10df9d866d
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-25 21:33:22 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,7 @@ export default defineConfig({
       maxConcurrentRoutes: 1,
       renderAfterTime: 500,
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: process.env.VERCEL ? '/tmp/chrome' : undefined, // Vercel 환경에서만 /tmp/chrome 사용
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // 실행 환경에 맞는 옵션
     },
     postProcess(renderedRoute) {
       renderedRoute.html = renderedRoute.html
