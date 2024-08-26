1af1629bd5cdff9e22a3933c5d4f3318ecd37af0
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-27 01:26:19 +0900

    t

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import svgr from "vite-plugin-svgr";
 import prerender from "@prerenderer/rollup-plugin";
 import path from "path";
 import { htmlPrerender } from "vite-plugin-html-prerender";
+import vitePrerender from 'vite-plugin-prerender-k'
 
 export default defineConfig({
-  plugins: [svgr(), htmlPrerender({
-    staticDir: path.join(__dirname, "dist"),
-    routes: ["/", "/test"],
-  })],
+  plugins: [svgr(), vitePrerender({
+    // Required - The path to the vite-outputted app to prerender.
+    staticDir: path.join(__dirname, 'dist'),
+    // Required - Routes to render.
+    routes: ['/', '/about', '/some/deep/nested/route'],
+  }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
