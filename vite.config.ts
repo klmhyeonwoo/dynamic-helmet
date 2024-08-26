e793d29d3c67240e149cdcac337a4dea038cc325
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-27 01:04:05 +0900

    tt

--- vite.config.ts
+++ vite.config.ts
@@ -8,31 +8,8 @@ import { htmlPrerender } from "vite-plugin-html-prerender";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), htmlPrerender({
-    /**
-     * Required: Output directory of `vite build`.
-     */
     staticDir: path.join(__dirname, "dist"),
-
-    /**
-     * Required: List of routes to pre-render.
-     */
     routes: ["/", "/test"],
-
-    /**
-     * Optional: Query selector to wait for. Defaults to `#root`.
-     */
-    selector: "main",
-
-    /**
-     * Optional: To minify html. Uses https://github.com/kangax/html-minifier.
-     */
-    minify: {
-      collapseBooleanAttributes: true,
-      collapseWhitespace: true,
-      decodeEntities: true,
-      keepClosingSlash: true,
-      sortAttributes: true
-    }
   })],
   resolve: {
     alias: {
