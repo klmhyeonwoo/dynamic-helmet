ff39c40d420cc88b44b7e940aa4f51032f04550f
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-27 00:58:46 +0900

    tt

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,37 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import prerender from "@prerenderer/rollup-plugin";
 import path from "path";
+import { htmlPrerender } from "vite-plugin-html-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), prerender({
+  plugins: [svgr(), htmlPrerender({
+    /**
+     * Required: Output directory of `vite build`.
+     */
+    staticDir: path.join(__dirname, "dist"),
+
+    /**
+     * Required: List of routes to pre-render.
+     */
     routes: ["/", "/test"],
-    renderer: "@prerenderer/renderer-puppeteer",
-    server: {
-      port: 3000,
-      host: "localhost",
-    },
-    rendererOptions: {
-      maxConcurrentRoutes: 1,
-      renderAfterTime: 500,
-    },
-    postProcess(renderedRoute) {
-      renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, "https:")
-          .replace(
-              /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
-              "https://dynamic-helmet.vercel.app/"
-          );
-    },
-  }),],
+
+    /**
+     * Optional: Query selector to wait for. Defaults to `#root`.
+     */
+    selector: "main",
+
+    /**
+     * Optional: To minify html. Uses https://github.com/kangax/html-minifier.
+     */
+    minify: {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      decodeEntities: true,
+      keepClosingSlash: true,
+      sortAttributes: true
+    }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
